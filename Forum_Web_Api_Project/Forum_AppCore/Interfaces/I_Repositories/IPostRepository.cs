43ae65f419e02b6f6ba09e46a81247e5a24db839
RaphRastelli <raphael.rastelli@gmail.com>
2026-02-27 16:05:09 +0100

    Basic Iservice + IRepository

---
+++ Forum_Web_Api_Project/Forum_AppCore/Interfaces/I_Repositories/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain_Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore_Forum.Interfaces.I_Repositories
+{
+    public interface IPostRepository
+    {
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllCommentByPost();
+        Post Create(Post data);
+        Post Update(Post data);
+
+
+    }
+}
---
+++ Forum_Web_Api_Project/Forum_AppCore/Interfaces/I_Services/IPostService.cs
@@ -0,0 +1,19 @@
+﻿using Domain_Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppCore_Forum.Interfaces.I_Services
+{
+    public interface IPostService
+    {
+        Post GetById(int id);
+
+        IEnumerable<Post> GetAllPosts();
+
+        Post Create(Post data);
+        Post Update(Post data);
+        Post Delete(int id);
+
+    }
+}
