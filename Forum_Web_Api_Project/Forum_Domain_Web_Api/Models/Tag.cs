2c9b3099dd1e769049c7c364d8f5e9fcdf864131
RaphRastelli <raphael.rastelli@gmail.com>
2026-02-27 13:09:26 +0100

    Models + ctor

--- Forum_Web_Api_Project/Forum_Domain_Web_Api/Models/Tag.cs
+++ Forum_Web_Api_Project/Forum_Domain_Web_Api/Models/Tag.cs
@@ -6,5 +6,24 @@ namespace Domain_Forum.Models
 {
     public class Tag
     {
+        public int Id { get; private set; }
+        public Post? Post { get; private set; }
+        public Comment? Comment { get; private set; }
+
+        // ctor
+        // - Empty for EFC
+        public Tag() { }
+
+        // - For creating a new tag from a post
+        public Tag(Post post)
+        {
+            Post = post ?? throw new ArgumentNullException(nameof(post));
+        }
+
+        // - For creating a new tag from a comment
+        public Tag(Comment comment)
+        {
+            Comment = comment ?? throw new ArgumentNullException(nameof(comment));
+        }
     }
 }
