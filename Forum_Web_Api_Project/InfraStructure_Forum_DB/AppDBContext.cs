c06e2bfae9dc2876da8867c0144fda013f128899
Alex <alexander.santillan7013@.com>
2026-02-27 14:14:35 +0100

    DBset

--- Forum_Web_Api_Project/InfraStructure_Forum_DB/AppDBContext.cs
+++ Forum_Web_Api_Project/InfraStructure_Forum_DB/AppDBContext.cs
@@ -1,9 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain_Forum.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace InfraStructure_Forum_DB
 {
     public class AppDBContext: DbContext
     {
+        #region Tables
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        #endregion
 
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*Authomated Collection of DataBase Configuration*/
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
