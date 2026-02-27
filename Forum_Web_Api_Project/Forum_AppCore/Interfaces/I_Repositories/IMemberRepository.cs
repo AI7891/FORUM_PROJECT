7cbd5b63d18b6a3fa2ed9512c00fcabcbafd5122
liudmyla53 <lutik.dim@gmail.com>
2026-02-27 15:48:06 +0100

    feat: add IMemberRepository to AppCore

---
+++ Forum_Web_Api_Project/Forum_AppCore/Interfaces/I_Repositories/IMemberRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain_Forum.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfraStructure_Forum_DB.Repositories
+{
+   public interface IMemberRepository
+    {
+        Member? GetById(int id);
+        Member Insert(Member data);
+        string? GetHashPwd(string email);
+        Member GetByEmail(string email);
+    }
+}
