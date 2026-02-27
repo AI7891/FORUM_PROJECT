d672eb8a232276321e390381e9c7d1804967e7be
liudmyla53 <lutik.dim@gmail.com>
2026-02-27 11:04:02 +0100

    Feature Comment Initiallized

--- Forum_Web_Api_Project/Forum_Domain_Web_Api/Models/Comment.cs
+++ Forum_Web_Api_Project/Forum_Domain_Web_Api/Models/Comment.cs
@@ -6,5 +6,29 @@ namespace Domain_Forum.Models
 {
     public class Comment
     {
+        // Identifiant unique du commentaire
+        public int Id { get; set; }
+        // Identifiant de l'auteur (Membre)
+        public Member MemberId { get; set; }
+        // Identifiant du post parent (Sujet)
+        public Post PostId { get; set; }
+        // Le texte du message (Contrainte : Éditer ses messages)
+        public string Content { get; set; }= string.Empty;
+        // Date de création pour le suivi du forum
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public IEnumerable<Like> Likes { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+        // Flag pour le signalement (Contrainte : Signaler un message)
+        public bool IsReported { get; set; }
+       // Nom du modérateur (Contrainte : Mention "Editer par...")
+        public string? EditedBy { get; set; }
+
+        public Comment()
+        {
+            
+        }
+
+
     }
 }
